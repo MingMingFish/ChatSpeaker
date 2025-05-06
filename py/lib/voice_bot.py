bdfd96237604cbcb8eeb8d54a57a66bdeba78c7b
MingMing <bridy0682@gmail.com>
2025-05-07 06:15:19 +0800

    fix bug
    fix join logic

--- py/lib/voice_bot.py
+++ py/lib/voice_bot.py
@@ -16,15 +16,14 @@ class VoiceBot:
         if ctx.author.voice is None: # 如果使用者不在語音頻道中
             return "請先加入語音頻道。"
         user_voice_channel = ctx.author.voice.channel
-        if self.voice_client is None: # 如果機器人不在語音頻道中
-            self.voice_client = await user_voice_channel.connect() # 連接到使用者的語音頻道
-            return f"已加入語音頻道：{self.voice_client.channel.name}"
-        if ctx.voice_client != user_voice_channel: # 如果機器人與使用者的頻道不同
-            if ctx.voice_client: # 如果機器人已經在語音頻道中
+        if self.voice_client:   # 如果機器人已經在語音頻道中
+            if(self.voice_client != user_voice_channel): # 如果在不同語音頻道
                 await self.voice_client.move_to(user_voice_channel) # 移動到使用者的語音頻道
-            return f"已加入語音頻道：{self.voice_client.channel.name}"
-        else:
-            return BOT_ALREADY_EXISTS_STR
+            else: # 如果機器人已經在使用者的語音頻道中
+                return BOT_ALREADY_EXISTS_STR
+        else: # 如果機器人不在語音頻道中，則連接到使用者的語音頻道
+            self.voice_client = await user_voice_channel.connect()
+        return f"已加入語音頻道：{self.voice_client.channel.name}"
 
     async def leave(self, ctx):
         """讓機器人離開語音頻道"""
@@ -70,8 +69,8 @@ class VoiceBot:
         else:
             await ctx.send(msg, delete_after = 3)
 
-        language = lang_detect.detect_language_for_gTTS(text)
-        audio = myTTS.get_audio(text, language)
+        language = await lang_detect.detect_language_for_gTTS(text)
+        audio = await myTTS.get_audio(text, language)
         await bot_audio.play_audio_sync(self.voice_client, audio)
         return None
     async def readout(self, ctx):
